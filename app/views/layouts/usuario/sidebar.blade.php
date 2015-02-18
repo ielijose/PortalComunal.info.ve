db6d579304fc15e0ad6003320b8f1c14a102e000
Eli José Carrasquero [Laptop] <ielijose@gmail.com>
2015-02-18 08:00:07 -0430

    Show questions again

--- app/views/layouts/usuario/sidebar.blade.php
+++ app/views/layouts/usuario/sidebar.blade.php
@@ -9,19 +9,10 @@
                 <a href="/mi-consejo-comunal"><i class="fa fa-columns"></i><span class="sidebar-text">Mi Consejo Comunal</span></a>
             </li>
 
-            {{--<li>
+            <li>
                 <a href="/preguntas"><i class="fa fa-question"></i><span class="sidebar-text">Preguntas</span></a>
             </li>
-    
-            @if(isset($current) && $current->estado == 'aceptado')
-            <li>
-                <a href="/documentos"><i class="fa fa-folder"></i><span class="sidebar-text">Documentos</span></a>
-            </li>
-
-            <li>
-                <a href="/calendario"><i class="fa fa-calendar"></i><span class="sidebar-text">Calendario</span></a>
-            </li>
-            @endif--}}
+            
             
             <br>
         </ul>
